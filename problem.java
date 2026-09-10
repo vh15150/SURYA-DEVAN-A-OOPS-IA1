96b2168fa21bce860843b2cdf4ee43fde7ebd847
vh15150 <vh15150@velhightech.com>
2026-09-10 11:27:51 +0530

    Implement Plan class for Problem A

--- problem.java
+++ problem.java
@@ -1,16 +1,47 @@
+
 class Plan {
     String planName;
     double dataLimitGB;
 
+    // Constructor
     Plan(String planName, double dataLimitGB) {
         this.planName = planName;
         this.dataLimitGB = dataLimitGB;
     }
 
-    public static void main(String[] args) {
-        Plan p = new Plan("5G Premium", 50.0);
+    // Display method
+    void display() {
+        System.out.println("Plan Name: " + planName);
+        System.out.println("Data Limit: " + dataLimitGB + " GB");
+    }
+}
 
-        System.out.println("Plan Name: " + p.planName);
-        System.out.println("Data Limit: " + p.dataLimitGB + " GB");
+// Subclass
+class PostpaidPlan extends Plan {
+    String billingCycle;
+
+    // Constructor
+    PostpaidPlan(String planName, double dataLimitGB, String billingCycle) {
+        super(planName, dataLimitGB);
+        this.billingCycle = billingCycle;
+    }
+
+    // Override display()
+    @Override
+    void display() {
+        System.out.println("Plan Name: " + planName);
+        System.out.println("Data Limit: " + dataLimitGB + " GB");
+        System.out.println("Billing Cycle: " + billingCycle);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        PostpaidPlan plan = new PostpaidPlan(
+            "5G Premium", 50.0, "Monthly"
+        );
+
+        plan.display();
     }
 }
