44bfb875dd5a9af8ba8e89a14ec6809e6f4767c9
vh15150 <vh15150@velhightech.com>
2026-09-16 22:07:53 +0530

    Refactor PrepaidRecharge message and rename class

--- problemB.java
+++ problemB.java
@@ -3,14 +3,15 @@ abstract class Recharge {
 }
 
 class PrepaidRecharge extends Recharge {
-    @Override
+
     void processRecharge() {
-        System.out.println("Prepaid recharge processed successfully.");
+        System.out.println("Prepaid recharge successful.");
     }
 }
 
-public class ProblemB {
+public class Main {
     public static void main(String[] args) {
+
         PrepaidRecharge p = new PrepaidRecharge();
         p.processRecharge();
     }
