a09b79c21b66609f1a5a3df57413a1b99da79a4b
vh15150 <vh15150@velhightech.com>
2026-09-16 21:59:53 +0530

    Refactor data limit validation in ProblemC

--- problemC.java
+++ problemC.java
@@ -1,20 +1,27 @@
+import java.util.Scanner;
+
 class InvalidDataLimitException extends Exception {
-    InvalidDataLimitException(String msg) {
-        super(msg);
+    InvalidDataLimitException(String message) {
+        super(message);
     }
 }
 
-public class ProblemC {
+public class Main {
     public static void main(String[] args) {
-        double dataLimit = -5;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter data limit: ");
+        int limit = sc.nextInt();
 
         try {
-            if (dataLimit < 0)
+            if (limit < 0) {
                 throw new InvalidDataLimitException(
                     "Data limit cannot be negative"
                 );
+            }
 
-            System.out.println("Valid Data Limit: " + dataLimit + " GB");
+            System.out.println("Valid data limit: " + limit);
+
         } catch (InvalidDataLimitException e) {
             System.out.println(e.getMessage());
         }
